4a52e5d5e0bbc1378631ee9e8b3f73cca898ef59
Claudiu Chirilov <cchirilov@adyax.com>
2017-04-13 15:06:01 +0300

    Added comments to the code + updated fields for article full view.

--- docroot/sites/all/themes/ddc_theme/templates/node/article/node--article--full.tpl.php
+++ docroot/sites/all/themes/ddc_theme/templates/node/article/node--article--full.tpl.php
@@ -12,6 +12,12 @@
       </div>
       <div class="media-body">
         <h1><?php print $title; ?></h1>
+        <h3><?php print render($content['field_article_subtitle']); ?></h3>
+        <h4><?php print render($content['field_article_publication_date']); ?></h4>
+        <div class="pull-left">
+          <?php print render($content['field_article_author_image']); ?>
+        </div>
+        <h5><?php print render($content['field_article_author_desc']); ?></h5>
         <?php print render($content['body']); ?>
       </div>
     </div>
